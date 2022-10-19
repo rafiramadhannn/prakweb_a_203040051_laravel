e6ca50f11527e87c83a551b5b9635b3e75c1ebac
rafiramadhannn <203040051@mail.unpas.ac.id>
2022-10-19 13:58:42 +0700

    membuat layouts dan partials

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view ('home');
+    return view ('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view ('about', [
+        "title" => "About",
         "name" => "Rafi Ramadhan",
         "email" => "rafiramadhan711@gmail.com",
         "image" => "rafi.jpeg"
@@ -26,5 +29,7 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view ('posts');
+    return view ('posts', [
+        "title" => "Posts"
+    ]);
 });
